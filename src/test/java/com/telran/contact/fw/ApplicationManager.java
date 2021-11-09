b0f2c5be188270e4839c2d27fc20ea67b2d4de8b
Mnv2017 <mnv2017@gmx.de>
2021-11-09 09:23:32 +0100

    Lesson 09.11.21 added browser selection added

--- src/test/java/com/telran/contact/fw/ApplicationManager.java
+++ src/test/java/com/telran/contact/fw/ApplicationManager.java
@@ -4,6 +4,8 @@ import com.telran.contact.tests.TestBase;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
@@ -12,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+     String browser;
 
 //    WebDriver driver;
 
@@ -20,6 +23,10 @@ public class ApplicationManager {
     UserHelper user; //объявили переменную для связи с хелпером
     ContactHelper contact; // --"--"--
     HomeHelper home;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public static class MyListener extends AbstractWebDriverEventListener {
         Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -44,7 +51,14 @@ public class ApplicationManager {
     }
 
     public void init() {
-        driver = new EventFiringWebDriver(new ChromeDriver());
+        if (browser.equals(BrowserType.CHROME)){
+            driver = new EventFiringWebDriver(new ChromeDriver());
+        } else if (browser.equals(BrowserType.FIREFOX)){
+            driver = new EventFiringWebDriver(new FirefoxDriver());
+        } else if (browser.equals(BrowserType.SAFARI)){
+            driver = new EventFiringWebDriver(new SafariDriver());
+        }
+
 //        driver = new ChromeDriver();
 //        driver = new FirefoxDriver();
         driver.get("https://contacts-app.tobbymarshall815.vercel.app");
